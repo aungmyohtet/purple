5734328256f5e233e65e5cdbf381e5ef519ea5f3
aungmyohtet <aungmyotet@outlook.com>
2017-02-10 10:07:04 +0630

    authentication filter added

---
+++ WebContent/login.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+ Login form new
+ <form action="login" method="post">
+  <input type="text" name="userName" placeholder="User Name"/>
+  <input type="password" name="password" placeholder="Password"/>
+  <input type="submit" value="Login"/>
+ </form>
+</body>
+</html>
---
+++ src/main/java/com/aungmyohtet/purple/web/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.aungmyohtet.purple.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userName = request.getParameter("userName");
+		request.getSession().setAttribute("userName", userName);
+		response.getWriter().append("Login successful");
+	}
+
+}
